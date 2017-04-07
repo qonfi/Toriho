c708620840ebfc68f5970fff8cd1176a91a81838
qonfi <confidentialrendezvous@gmail.com>
2017-04-07 22:58:15 +0900

    Add files via upload

---
+++ GroundDetector2DDebugger.cs
@@ -0,0 +1,31 @@
+﻿
+using UnityEngine;
+using Fiziks2D;
+using System;
+
+namespace IMGUI
+{
+
+
+    public class GroundDetector2DDebugger : MonoBehaviour, IDebuggerRow
+    {
+        [SerializeField] private GroundDetector2D detector;
+        public bool Visible { get; set; }
+        
+
+        public void Display()
+        {
+            if (detector == null)
+            {
+                GUILayout.Box("GroundDetector2D == null");
+                return;
+            }
+
+            GUILayout.Box("IsGrounding\n" + detector.IsGrounding);
+            GUILayout.Box("___LastDetectedGround___\n" + detector.LastDetectedGround + "\n" +
+                "__GroundBeingDetected__\n" + detector.GroundBeingDetected);
+        }
+    }
+
+
+}
