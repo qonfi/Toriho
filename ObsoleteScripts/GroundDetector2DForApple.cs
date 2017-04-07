2349bfb86a3d4ceef867274792aea87ab5408a16
qonfi <confidentialrendezvous@gmail.com>
2017-04-07 22:49:53 +0900

    Add files via upload

---
+++ ObsoleteScripts/GroundDetector2DForApple.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using UnityEngine;
+
+namespace Fiziks2D
+{
+    public class GroundDetector2DForApple : MonoBehaviour, IGroundDetector2D
+    {
+        public GameObject GroundBeingDetected { get; set; }
+        public GameObject LastDetectedGround { get; set; }
+        public bool IsGrounding { get; set; }
+        
+        private float Radius { get; set; }
+        public IRayCaster2D Caster { get; set; }
+        private bool Bouncing { get; set; }
+
+
+        private void Start()
+        {
+            Radius = 0.5f;
+            Caster = new OverlapDetectorCircle2D(Radius);
+        }
+
+
+        private void Update()
+        {
+            IsGrounding = Caster.Cast(transform.position);
+            GroundBeingDetected = Caster.ObjectBeingDetected;
+            LastDetectedGround = Caster.LastDetectedObject;
+        }
+
+    }
+}
---
+++ ObsoleteScripts/GroundDetectorForInorganic2D.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using UnityEngine;
+
+namespace Fiziks2D
+{
+
+    // 名前長過ぎ
+    public class GroundDetectorForInorganic2D : MonoBehaviour, IGroundDetector2D
+    {
+        public GameObject GroundBeingDetected { get; set; }
+        public GameObject LastDetectedGround { get; set; }
+        //public Vector2 MovementPerFrame { get; set; }
+        public bool IsGrounding { get; set; }
+        public IRayCaster2D Caster { get; set; }
+
+        private float Radius { get; set; }
+
+
+        private void Start()
+        {
+            // テスト用
+            Radius = 0.5f;
+        }
+
+        private void Update()
+        {
+            Collider2D other;
+            other = Physics2D.OverlapCircle(transform.position, Radius);
+
+            Debug.Log(other);
+        }
+    }
+}
